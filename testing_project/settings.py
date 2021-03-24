4577a61cb329d8fef378ff34a174e96b60e5df11
FENR1 USER <fenr1user@biadsconsulting.org>
2021-03-24 17:29:23 +0600

    python collect

--- testing_project/settings.py
+++ testing_project/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -117,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL= '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 
 # CACHES = {
 #     "default": {
