5f869c99c134f08993fcbefad1752c061784b749
FENR1 USER <fenr1user@biadsconsulting.org>
2021-03-24 17:33:20 +0600

    static

--- testing_project/settings.py
+++ testing_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
@@ -116,8 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL= '/static/'
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/Project/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # CACHES = {
 #     "default": {
 #         "BACKEND": "django_redis.cache.RedisCache",
