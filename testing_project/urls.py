2b99a550dd092759319103df6278be5ef095014d
FENR1 USER <fenr1user@biadsconsulting.org>
2021-03-24 16:28:09 +0600

    static

--- testing_project/urls.py
+++ testing_project/urls.py
@@ -5,5 +5,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app1.urls'))
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
